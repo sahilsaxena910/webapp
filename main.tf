553d7cc7b0b464da6e85e69176f235b2b2bf869b
Sahil Saxena <sahilsaxena047@gmail.com>
2024-02-15 22:44:32 +0530

    Added route table

--- main.tf
+++ main.tf
@@ -42,6 +42,19 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_cidr_blocks)
   subnet_id      = aws_subnet.public[count.index].id
