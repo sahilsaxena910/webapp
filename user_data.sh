9a41b60cde4d47653620a183a5c3d86175355381
Sahil Saxena <sahilsaxena047@gmail.com>
2024-02-17 17:59:50 +0530

    Added ansible installation process in userdata script and added web_app_web.yml which is a ansible playbook

--- user_data.sh
+++ user_data.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+yum update -y
+
 mkfs -t ext4 /dev/xvdf
 mkdir /var/log/myapp
 mount /dev/xvdf /var/log/myapp
 echo '/dev/xvdf /var/log/myapp ext4 defaults,nofail 0 2' >> /etc/fstab
 
 # Install Nginx
-yum update -y
-amazon-linux-extras install nginx1.12 -y
+yum install nginx -y
 
 # Start Nginx
 systemctl start nginx
-systemctl enable nginx
+systemctl enable 
+
+## Install Ansible
+
+yum install ansible -y
