f09bff5a879db7412a622b50b2107d947c53814e
Sahil Saxena <sahilsaxena047@gmail.com>
2024-02-18 00:42:07 +0530

    Added deploy via docker (nginx will reside in container)or simply using ansible and nginx reside on plain server as opposed in a container feature

--- variables.tf
+++ variables.tf
@@ -13,3 +13,7 @@ variable "public_subnet_cidr_blocks" {
 variable "private_subnet_cidr_blocks" {
   description = "list of cidr block for private subnet(s). Enter more than one cidr if you want multiple cidr blocks"
 }
+variable "isDocker" {
+  type = bool
+  default = false
+}
